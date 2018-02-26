d803ba307d1afd774e026f6e0e0f7e5fc2319a83
Tyler Bray <broncobray77@gmail.com>
2018-02-26 13:37:17 -0800

    darn whitespace

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -15,12 +15,30 @@ import React from 'react';
 // }
 
 const Weather = (props) => (
-    <div>
-        {props.city && props.country && <div>Location: {props.city}, {props.country} </div>}
-        {props.temperature && <div> Temperature: {props.temperature} </div>}
-        {props.humidity && <div> Humidity: {props.humidity} </div>}
-        {props.description && <div> Weather Conditions: {props.description} </div>}
-        {props.error && <div> {props.error} </div>}
+    <div className="weather__info">
+        {
+            props.city && props.country && <div className="weather__key"> Location: 
+                <span className="weather__value"> {props.city}, {props.country} </span>
+            </div>
+        }
+        {
+            props.temperature && <div className="weather__key"> Temperature:
+                <span className="weather__value"> {props.temperature} </span>
+            </div>
+        }
+        {
+            props.humidity && <div className="weather__key"> Humidity: 
+                <span className="weather__value"> {props.humidity} </span>
+            </div>
+        }
+        {
+            props.description && <div className="weather__key"> Weather Conditions:
+                <span className="weather__value"> {props.description} </span>
+            </div>
+        }
+        {
+            props.error && <div> {props.error} </div>
+        }
     </div>
 );
 
--- src/components/titles.js
+++ src/components/titles.js
@@ -1,14 +1,21 @@
 import React from 'react';
 
-class Titles extends React.Component {
-    render() {
-        return (
-            <div>
-                <h1>Weather Finder</h1>
-                <p>Find out temp, conditions and more!</p>
-            </div>
-        );
-    }
-}
+// class Titles extends React.Component {
+//     render() {
+//         return (
+//             <div>
+//                 <h1>Weather Finder</h1>
+//                 <p>Find out temp, conditions and more!</p>
+//             </div>
+//         );
+//     }
+// }
+
+const Titles = () => (
+    <div>
+        <h1 className="title-contain__title">Weather Finder</h1>
+        <h3 className="title-contain__subtitle">Find out temp, conditions and more!</h3>
+    </div>
+)
 
 export default Titles;
